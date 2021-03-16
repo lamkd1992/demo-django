9e9e19153f3d52a982374b8e8a6d84666867ee48
Kim Lam <lamkd@miagi-so.net>
2021-03-16 17:27:05 +0700

    Add bootstrap library and home's view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.writelines('<h1>Hello!!!</h1>')
-    response.writelines('This is home app!!!')
-
-    return response
+    return render(request, 'pages/home.html')
